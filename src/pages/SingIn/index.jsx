ee99526d85e6ca30d83acb551f5827ff52e71e66
Warlley Lopes <warlleylopes123@gmail.com>
2024-05-04 18:47:05 +0100

    implementação page singin

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,11 +1,25 @@
-import {Container, Form, Background} from './styles'
-import {Button} from '../../components/Button'
+import { useState } from 'react'
 import {FiMail, FiLock} from 'react-icons/fi'
-import {Input} from '../../components/Input'
 import { Link } from 'react-router-dom'
+
+import { useAuth } from '../../hooks/auth'
+
+import {Input} from '../../components/Input'
+import {Button} from '../../components/Button'
+
+import {Container, Form, Background} from './styles'
 
 
 export function SingIn(){
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const {singIn} = useAuth()
+
+    function handleSingIn(){
+        singIn({email, password})
+    }
+
     return(
         <Container>
             <Form>
@@ -15,10 +29,21 @@ export function SingIn(){
 
             <h2>Faça seu login</h2>
 
-            <Input placeholder='E-mail' type="text" icon={FiMail}/>
-            <Input placeholder='Password' type="password" icon={FiLock}/>
+            <Input 
+                placeholder='E-mail' 
+                type="text" 
+                icon={FiMail} 
+                onChange={e => setEmail(e.target.value)}
+            />
 
-            <Button title="Entrar"/>
+            <Input 
+                placeholder='Password' 
+                type="password" 
+                icon={FiLock} 
+                onChange={e => setPassword(e.target.value)}
+            />
+
+            <Button title="Entrar" onClick={handleSingIn}/>
 
             <Link to='/register'>Criar conta</Link>
 
